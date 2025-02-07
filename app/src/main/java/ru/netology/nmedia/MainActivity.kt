de03a47b1a38d19ad8b1d1ee17b2ed86125f9678
Danil <danil.ilin.geo@gmail.com>
2025-02-07 04:15:15 +0300

    Изменил UI

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,12 +1,18 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private var likeCount = 0
+    private var isLiked = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,45 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+
+        val likeButton: ImageButton = findViewById(R.id.likeButton)
+        val likeCountText: TextView = findViewById(R.id.likeCount)
+
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        likeCount = prefs.getInt("like_count", 0)
+        isLiked = prefs.getBoolean("is_liked", false)
+
+        likeCountText.text = likeCount.toString()
+        setLikeState(likeButton, likeCountText, isLiked)
+
+        likeButton.setOnClickListener {
+            isLiked = !isLiked
+
+            if (isLiked) {
+                likeCount++
+            } else {
+                likeCount--
+            }
+
+            setLikeState(likeButton, likeCountText, isLiked)
+            saveLikeState()
+        }
+    }
+
+    private fun setLikeState(button: ImageButton, textView: TextView, liked: Boolean) {
+        val color = if (liked) R.color.red else R.color.gray
+        button.setColorFilter(ContextCompat.getColor(this, color))
+        textView.text = likeCount.toString()
+    }
+
+    private fun saveLikeState() {
+        val prefs = getSharedPreferences("prefs", MODE_PRIVATE)
+        with(prefs.edit()) {
+            putInt("like_count", likeCount)
+            putBoolean("is_liked", isLiked)
+            apply()
         }
     }
 }
